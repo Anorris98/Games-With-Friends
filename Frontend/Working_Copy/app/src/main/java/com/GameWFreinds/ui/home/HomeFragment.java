2eba8262262bf6575e0384e1b8b1fc0c1c3bb4d4
Anorris <anorris+Git@iastate.edu>
2024-02-19 12:43:05 -0600

    Layout bar now has links to Account, Play, And messages tab fragments

--- Frontend/Working_Copy/app/src/main/java/com/GameWFreinds/ui/home/HomeFragment.java
+++ Frontend/Working_Copy/app/src/main/java/com/GameWFreinds/ui/home/HomeFragment.java
@@ -10,18 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.GameWFreinds.databinding.FragmentHomeBinding;
+import com.GameWFreinds.databinding.FragmentPlayBinding;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentPlayBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentPlayBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
