cf1adf6a872f33942b242368df47d938334b090d
Anorris <anorris+Git@iastate.edu>
2024-02-21 10:10:27 -0600

    Account, Play, Messages, and notifications fragments all working with navigation now. Renamed and ready for Implementation of buttons etc.

--- Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/Messages/MessagesFragment.java
+++ Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/Messages/MessagesFragment.java
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.GameWFriends.databinding.FragmentDashboardBinding;
+import com.GameWFriends.databinding.FragmentMessagesBinding;
 
-public class DashboardFragment extends Fragment {
+public class MessagesFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentMessagesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        MessagesViewModel messagesViewModel =
+                new ViewModelProvider(this).get(MessagesViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentMessagesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textMessages;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        messagesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/Messages/MessagesViewModel.java
+++ Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/Messages/MessagesViewModel.java
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class MessagesViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public MessagesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is Messages fragment");
     }
 
     public LiveData<String> getText() {
