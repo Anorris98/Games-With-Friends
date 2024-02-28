3db857203d63c49c3b219933b47bb707830c580e
anorris <anorris@iastate.edu>
2024-02-27 19:56:00 -0600

    Added more generic Volley requests, they also now work with json objects only. The old string versions can be found as a .bak in the backup folder. I also have some methods for calling and manipulating the user tables loaded. Added GUI aspects that allow a user to specify multiple things when wanting to manipulate stuff from admin tool page. Added a Constants.java for constants we use over and over on the frontend like the url, which can be accessed using Constants.BASE_URL. Still a work in progress. Updated the Get request to require the full url when used.

--- Frontend/Working_Copy/app/src/main/java/com/GameWFriends/VolleyAPIService.java
+++ Frontend/Working_Copy/app/src/main/java/com/GameWFriends/VolleyAPIService.java
@@ -2,16 +2,20 @@ package com.GameWFriends;
 
 import android.content.Context;
 
+import com.GameWFriends.ui.AdminTools.Constants;
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 //Example of how to use this class in other activites:
@@ -31,7 +35,8 @@ import org.json.JSONObject;
 
 public class VolleyAPIService {
 
-    private String url = "https://59a7d876-383e-468e-8b60-f3636af76d88.mock.pstmn.io/test";
+    //made equal to base as to not have to refactor this code.
+    private String url = Constants.BASE_URL;
     private RequestQueue requestQueue; // Volley request queue
 
     // Constructor
@@ -42,65 +47,105 @@ public class VolleyAPIService {
     // Interface for GET request callbacks
     public interface VolleyResponseListener {
         void onError(String message);
-        void onResponse(String response);
+        void onResponse(JSONObject response);
     }
 
-    // GET Request
-    public void getRequest(final String userId, final VolleyResponseListener listener) {
-        String urlWithId = url + "/users/" + userId; // Assuming 'url' is the base URL ending before '/users/'
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlWithId,
-                new Response.Listener<String>() {
+    /**GetRequest function, allows a get request to be sent using a json object.
+     *
+     * @param finalUrl the full final url that will be getting sent
+     * @param listener Listener instance
+    */
+    public void getRequest(final String finalUrl, final VolleyResponseListener listener) {
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, finalUrl, null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONObject response) {
+                        listener.onResponse(response); //pass string value
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String errorMessage = "That didn't work!";
+                        if (error.getMessage() != null) {
+                            errorMessage += " " + error.getMessage();
+                        }
+                        listener.onError(errorMessage);
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                // if we end up needing headers later: headers.put("Authorization", "Bearer " + YOUR_TOKEN); or something like that.
+                return headers;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    // POST Request
+    public void postRequest(final String url, final JSONObject requestBody, final VolleyResponseListener listener) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, requestBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
                         listener.onResponse(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        listener.onError("That didn't work!" + error.toString());
+                        // Handle error
+                        String errorMessage = error.getMessage();
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
+                            errorMessage += "\n" + new String(error.networkResponse.data);
+                        }
+                        listener.onError(errorMessage);
                     }
-                });
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
 
-        requestQueue.add(stringRequest);
+                headers.put("Content-Type", "application/json");
+                // if we end up needing headers later: headers.put("Authorization", "Bearer " + YOUR_TOKEN); or something like that.
+                return headers;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
     }
 
-    // POST Request
-    public void postRequest(final String requestBody, final VolleyResponseListener listener) throws JSONException {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url,
-                requestBody.isEmpty() ? null : new JSONObject(requestBody),
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        listener.onResponse(response.toString());
+//    // DELETE Request
+    public void deleteRequest(final String userId, final VolleyResponseListener listener) {
+        String urlWithId = url + "/users/" + userId; // Adjust URL to target specific resource for deletion
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, urlWithId, null,
+                response -> {
+                    try {
+                        listener.onResponse(new JSONObject().put("message", "Deleted Successfully."));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        listener.onError(error.getMessage());
-                    }
-                });
+                error -> listener.onError("Delete Failed: " + error.toString())
+        );
 
         requestQueue.add(jsonObjectRequest);
     }
-    // DELETE Request
-    public void deleteRequest(final VolleyResponseListener listener) {
-        StringRequest stringRequest = new StringRequest(Request.Method.DELETE, url,
-                response -> listener.onResponse("Deleted Successfully."),
-                error -> listener.onError("Delete Failed: " + error.toString()));
 
-        requestQueue.add(stringRequest);
-    }
-
-    // PUT Request
-    public void putRequest(final String requestBody, final VolleyResponseListener listener) throws JSONException {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url,
-                requestBody.isEmpty() ? null : new JSONObject(requestBody),
-                response -> listener.onResponse(response.toString()),
-                error -> listener.onError(error.getMessage()));
+//    // PUT Request
+    public void putRequest(final JSONObject requestBody, final VolleyResponseListener listener) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url, requestBody,
+                response -> listener.onResponse(response),
+                error -> listener.onError(error.getMessage())
+        );
 
         requestQueue.add(jsonObjectRequest);
     }
+
 }
--- Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/AdminTools/AdminToolsUserTable.java
+++ Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/AdminTools/AdminToolsUserTable.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,9 +14,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-
 import com.GameWFriends.R;
 import com.GameWFriends.VolleyAPIService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AdminToolsUserTable extends Fragment {
 
@@ -55,40 +58,179 @@ public class AdminToolsUserTable extends Fragment {
     }
 
     private void setupListeners(View view) {
-        //text box declarations
-//        TextView textViewResponse = view.findViewById(R.id.Textview_Response);  //text view for string response
+//      Edit text declarations delete when done with this part
+        EditText numberUserIDGroupID = view.findViewById(R.id.editTextNumberID);        // texview: Userid/GroupID
+        EditText emailEmailAddress  = view.findViewById(R.id.editTextTextEmailAddress); // texview: EmailAddress
+        EditText textUsername       = view.findViewById(R.id.editTextUsername);         // texview: Username
+        EditText passwordPassword   = view.findViewById(R.id.editTextPassword);         // texview: Password
+
+        //edit text declarations for later use and getting values.
+//        int userGroupID;
+//        String emailAddress;
+//        String username;
+//        String password;
+
+
+        //convert to strings and usable int.
+//        int userIDGroupID = Integer.parseInt(numberUserIDGroupID.getText().toString());
+//        String emailAddress = emailEmailAddress.getText().toString();
+//        String username = textUsername.getText().toString();
+//        String password = passwordPassword.getText().toString();
+
+
 
         //button declarations
-        Button buttonViewUserInfo = view.findViewById(R.id.buttonViewUserInfo); // View user Info button
-        Button loginButton = view.findViewById(R.id.buttonLogin);               // login button
-        Button buttonRegister = view.findViewById(R.id.buttonRegister);         // Register Button
+        Button buttonRegister = view.findViewById(R.id.buttonRegister);             // button: Register
+        Button buttonLogin = view.findViewById(R.id.buttonLogin);                   // button: login
+        Button buttonViewUserInfo = view.findViewById(R.id.buttonViewUserInfo);     // button: View current user info
+        Button buttonUpdateUser = view.findViewById(R.id.buttonUpdateUser);         // button: Updated User account information, display name, Profile Picture, Bio/description
+        Button buttonUpdatePassword = view.findViewById(R.id.buttonUpdatePassword); // button: update Password/change password
+        Button buttonDeleteUser = view.findViewById(R.id.buttonDeleteUser);         // button: User Account Delete
 
-        //listeners start here.
-        buttonViewUserInfo.setOnClickListener(new View.OnClickListener() {
+        //listeners start here
+        //TODO: set up registration button.
+        buttonRegister.setOnClickListener(new View.OnClickListener() {          //register New user
             @Override
             public void onClick(View v) {
-                // Call getRequest from VolleyAPIService
-               fetchUserProfile(69);
+                //assign to strings
+                String emailAddress = emailEmailAddress.getText().toString();
+                String password = passwordPassword.getText().toString();
+
+                //call method to handle this part
+                registerUser(emailAddress, password);
 
             }
         });
 
-        // Setup similar click listeners for the rest.
+        //Todo Setup ability to log in/return the user info
+        buttonLogin.setOnClickListener(new View.OnClickListener() {             // Login
+            @Override
+            public void onClick(View v) {
+            //take username and password and set it using a post, depending on return, we can act on logging in or logging out.
+            }
+        });
+
+
+        buttonViewUserInfo.setOnClickListener(new View.OnClickListener() {      // View User info
+            @Override
+            public void onClick(View v) {
+                //get int value
+                int userIDGroupID;
+
+                try {
+                    userIDGroupID = Integer.parseInt(numberUserIDGroupID.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getContext(), "Error: please Enter a number ", Toast.LENGTH_LONG).show();
+
+                    //TODO: make sure this is is changed to the correct return code.
+                    //just incase set it to error 402
+                    userIDGroupID = 402;
+                }
+
+
+                // Call getRequest from VolleyAPIService
+               fetchUserProfile(userIDGroupID);
+            }
+        });
+
+        //todo: ALlow user info to be updated
+        buttonUpdateUser.setOnClickListener(new View.OnClickListener() {        // Update user info
+            @Override
+            public void onClick(View v) {
+                //getuser id field and Put the payload and ship it to the front end.
+
+            }
+        });
+
+        //Todo: Allow a user/us to change account passwords
+        buttonUpdatePassword.setOnClickListener(new View.OnClickListener() {    //Update User Password
+            @Override
+            public void onClick(View v) {
+//                Need userID to send, then must send OldPwd, and new password to server.
+
+            }
+        });
+
+        //Todo: Allow us or the user to delete their account from the database.
+        buttonDeleteUser.setOnClickListener(new View.OnClickListener() {        //Delete user
+            @Override
+            public void onClick(View v) {
+        //get userID and password, load into payload and send it using the UserID and delete to front end.
+            }
+        });
+
     }
 
 
+    /**
+     * Retrieves the profile for a specified user ID.
+     *
+     * @param userId the ID of the user whose profile is to be fetched.
+     */
     public void fetchUserProfile(int userId) {
-        apiService.getRequest(String.valueOf(userId), new VolleyAPIService.VolleyResponseListener() {
+        // Assuming 'baseUrl' is a String variable holding your API's base URL
+        String baseUrl = "https://your.api.url"; // Replace with your actual base URL
+        String finalUrl = Constants.BASE_URL + "/users/" + userId; // Construct the full URL
+
+        apiService.getRequest(finalUrl, new VolleyAPIService.VolleyResponseListener() {
             @Override
             public void onError(String message) {
+                // Display error message
                 Toast.makeText(getContext(), "Error: " + message, Toast.LENGTH_LONG).show();
             }
 
             @Override
-            public void onResponse(String response) {
+            public void onResponse(JSONObject response) {
+                try {
+                    // Convert the JSONObject to a String with indentation for formatting
+                    String formattedResponse = response.toString(4); // Indent with 4 spaces
+
+                    // Display the formatted JSON response
+                    mViewModel.setResponse("Response is:\n" + formattedResponse);
+                } catch (JSONException e) {
+                    // Handle JSON parsing error
+                    Toast.makeText(getContext(), "Error handling JSON", Toast.LENGTH_LONG).show();
+                }
+                // Optionally, you might want to move or remove this success toast to suit your flow
                 Toast.makeText(getContext(), "Success", Toast.LENGTH_LONG).show();
-                mViewModel.setResponse("Response is:" + response);
             }
         });
     }
+
+
+
+
+    /**
+     * Registers a user.
+     *
+     * @param email the email of the new user..
+     * @param password the password the person wants to register
+     */
+    //TODO: Markus refused to add username to registration call, so we will have to update this to take in a username as well, then make a call to set the username.
+    public void registerUser(String email, String password) {
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("email", email);
+            postData.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Error creating JSON object for registration", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        apiService.postRequest("register_endpoint", postData, new VolleyAPIService.VolleyResponseListener() {
+            @Override
+            public void onError(String message) {
+                Toast.makeText(getContext(), "Registration Error: " + message, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getContext(), "Registration Success", Toast.LENGTH_LONG).show();
+                mViewModel.setResponse("Registration response: " + response);
+            }
+
+        });
+    }
+
 }
