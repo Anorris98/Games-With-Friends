44d20e40fb0615b34ac714c37d85749c230002f1
Zac <zasears@iastate.edu>
2024-02-28 11:20:16 -0600

    AccessRole and its controller classes completed. Needs testing, but all api defined requests created.

--- Backend/GamesWithFriends/src/main/java/Server/Access_Roles/AccessRole.java
+++ Backend/GamesWithFriends/src/main/java/Server/Access_Roles/AccessRole.java
@@ -23,8 +23,36 @@ public class AccessRole
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int ID;
 
-    //Role title
-    private String name;
+    //Id regarded user
+    private int userId;
 
+    //Id of the role asigned to the user
+    private int roleId;
 
+    public AccessRole(int id, int userId, int roleId)
+    {
+        this.ID = id;
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public void setRoleIdq(int newId)
+    {
+        roleId = newId;
+    }
+
+    public int getRoleId()
+    {
+        return roleId;
+    }
 }
