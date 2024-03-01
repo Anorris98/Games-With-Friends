0d3672518fb07a179f946a859b781ccac55e7260
anorris <anorris@iastate.edu>
2024-03-01 04:42:32 -0600

    Tested with local backend server, all calls work for updating and changing user tables. as well as friend group tables. Some small issues on backend, but other than that, the functionality is implemented and ready for demo2.

--- Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/AdminTools/Constants.java
+++ Frontend/Working_Copy/app/src/main/java/com/GameWFriends/ui/AdminTools/Constants.java
@@ -1,5 +1,6 @@
 package com.GameWFriends.ui.AdminTools;
 
 public class Constants {
-    public static final String BASE_URL = "https://59a7d876-383e-468e-8b60-f3636af76d88.mock.pstmn.io";
+//    public static final String BASE_URL = "https://59a7d876-383e-468e-8b60-f3636af76d88.mock.pstmn.io";
+    public static final String BASE_URL = "http://10.0.2.2:8080";
 }
